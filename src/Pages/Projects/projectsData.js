54eb2436aace3f7782ee90bb6d682de3d2def257
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-06-20 16:48:05 +0530

    Updating Projects And its Details....

--- src/Pages/Projects/projectsData.js
+++ src/Pages/Projects/projectsData.js
@@ -134,7 +134,7 @@ const projectsData = [
       "React Toastify",
       "Render",
       "Vercel",
-      "MongoDB (Optional)",
+      "MongoDB",
       "dotenv",
       "CORS",
       "express-fileupload",
